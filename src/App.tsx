50742965949fd4060e391f91f1e315e57c5f5222
GlassesUSA <theo.f@glassesusa.com>
2022-12-05 18:20:09 +0200

    update 4 files, create 25 files, delete 1 file and rename 4 files

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <iframe src="./face_test/index.html" width="100%" height="1920px" />
+    </div>
+  )
+}
+
+export default App
